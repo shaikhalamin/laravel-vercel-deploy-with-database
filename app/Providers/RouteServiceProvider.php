7951371499a3c504d62d748a30f26f8fc30520f0
Shaikh Al Amin <alamin@tikweb.com>
2024-05-21 04:47:07 +0600

    vite build drop for api build issue

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -31,7 +31,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('v1/api')
-                ->group(base_path('routes/api.php'));
+                ->group(base_path('routes/api.php'));   
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
